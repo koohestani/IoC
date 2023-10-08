536ccee702303331d910ee5d29cf38c1a3be116f
hamed koohestani <hamedk9776@gmail.com>
2023-10-08 07:44:12 +0000

    dont repeat yourself!

--- include/Container.h
+++ include/Container.h
@@ -40,39 +40,13 @@ public:
     template <NotParameterized T>
     std::shared_ptr<T> resolve()
     {
-        if (!_factoryMap.contains(typeid(T)))
-            throw std::runtime_error { "Could not find generator for type [" + std::string { typeid(T).name() }
-                + "] in IoC container" };
-
-        try
-        {
-            return std::any_cast<Generator<T>>(_factoryMap.at(typeid(T)))();
-        }
-        catch (const std::bad_any_cast&)
-        {
-            throw std::runtime_error { "Could not resolve services mapped type\n from: ["
-                + std::string { _factoryMap.at(typeid(T)).type().name() } + "]\n to: ["
-                + std::string { typeid(Generator<T>).name() } + "]." };
-        }
+        return resolve<T, Generator<T>>();
     }
 
     template <Parameterized T>
-    std::shared_ptr<T> resolve(typename T::Params params = {})
+    std::shared_ptr<T> resolve(typename T::Params&& params = {})
     {
-        if (!_factoryMap.contains(typeid(T)))
-            throw std::runtime_error { "Could not find generator for type [" + std::string { typeid(T).name() }
-                + "] in IoC container" };
-
-        try
-        {
-            return std::any_cast<ParameterizedGenerator<T>>(_factoryMap.at(typeid(T)))(std::move(params));
-        }
-        catch (const std::bad_any_cast&)
-        {
-            throw std::runtime_error { "Could not resolve services mapped type\n from: ["
-                + std::string { _factoryMap.at(typeid(T)).type().name() } + "]\n to: ["
-                + std::string { typeid(ParameterizedGenerator<T>).name() } + "]." };
-        }
+        return resolve<T, ParameterizedGenerator<T>>(std::forward<typename T::Params>(params));
     }
 
     static Container& get()
@@ -82,5 +56,24 @@ public:
     }
 
 private:
+    template <class T, class G, class... Ps>
+    std::shared_ptr<T> resolve(Ps&&... args)
+    {
+        if (!_factoryMap.contains(typeid(T)))
+            throw std::runtime_error { "Could not find generator for type [" + std::string { typeid(T).name() }
+                + "] in IoC container" };
+
+        try
+        {
+            return std::any_cast<G>(_factoryMap.at(typeid(T)))(std::forward<Ps>(args)...);
+        }
+        catch (const std::bad_any_cast&)
+        {
+            throw std::runtime_error { "Could not resolve services mapped type\n from: ["
+                + std::string { _factoryMap.at(typeid(T)).type().name() } + "]\n to: ["
+                + std::string { typeid(G).name() } + "]." };
+        }
+    }
+
     std::unordered_map<std::type_index, std::any> _factoryMap;
 };
